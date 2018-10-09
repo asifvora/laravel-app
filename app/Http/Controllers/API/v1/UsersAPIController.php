550041db0ce9cc102aad64a3702cb90d9d57dc61
asif <asif.vora@multidots.com>
2018-10-09 19:37:11 +0530

    Implementing a swagger document for the API.

--- app/Http/Controllers/API/v1/UsersAPIController.php
+++ app/Http/Controllers/API/v1/UsersAPIController.php
@@ -9,10 +9,46 @@ use Illuminate\Http\Response;
 class UsersAPIController extends Controller
 {
     /**
-     * Show the application users.
-     *
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/api/v1/users",
+     *     operationId="usersList",
+     *     tags={"Users"},
+     *     summary="Get list of users",
+     *     description="Returns list of users",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="ValidationError"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Forbidden"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="NotFound"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation failed"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error"
+     *     ),
+     * )
      */
+
     public function index(Request $request)
     {
         $users = User::orderBy('created_at', 'asc')->get();
